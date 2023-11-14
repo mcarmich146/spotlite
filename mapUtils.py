370dfec18470725de453bb337d75a147de7202aa
Stuart Carmichael <stuart@carzari.com>
2023-11-13 19:47:29 -0800

    applied PEP8 and linting to mapUtils.py

--- mapUtils.py
+++ mapUtils.py
@@ -5,28 +5,40 @@
 # This file is subject to the terms and conditions defined in the file 'LICENSE',
 # which is part of this source code package.
 
-import config
-import folium
-from folium import raster_layers
-import plotly.graph_objs as go
-import plotly.express as px
-import branca.colormap as cm
-import geopandas as gpd
-import pandas as pd
-from shapely.geometry import shape, box
-from geopy.distance import distance
-from shapely.ops import unary_union
+"""Folium-based map utilities for creating heatmaps for cloud coverage, age of data, etc."""
+
+from typing import Tuple, Dict, Optional, List
+import sys
+from datetime import datetime
 
 import logging
-from satellogicUtils import group_by_capture
-import pyproj
-from pyproj.exceptions import CRSError
+
+import folium
+from folium import raster_layers
+
+import plotly.graph_objs as go
+import plotly.express as px
+
+import branca.colormap as cm
+
+import geopandas as gpd
+import pandas as pd
+
+from geopy.distance import distance
+
 from shapely import Point
+from shapely.ops import unary_union
+from shapely.geometry import shape, box
+
+from satellogicUtils import group_by_capture
+
+import config
 
 logger = logging.getLogger(__name__)
 
 def estimate_zoom_level(minx, miny, maxx, maxy):
-    # calculate the geographic extent
+    """Calculate the geographic extent."""
+
     width = maxx - minx
     height = maxy - miny
 
@@ -57,15 +69,24 @@ def estimate_zoom_level(minx, miny, maxx, maxy):
     # print(f"Zoom: {zoom_level}")
     return zoom_level
 
-def create_bounding_box(center_lat, center_lon, width_km=3):
-    # Calculate the height based on the width to maintain a 16:9 aspect ratio
+
+def create_bounding_box(center_lat: float,
+                        center_lon: float,
+                        width_km: float = 3) -> Tuple[float, float, float, float]:
+    """Create bounding box based on center and width."""
+
+    # Calculate the height based on the width to maintain a 16:9 aspect ratio.
     height_km = width_km * 9 / 16
 
     # Calculate the deltas in km for each direction
-    north_point = distance(kilometers=height_km/2).destination(point=(center_lat, center_lon), bearing=0)
-    south_point = distance(kilometers=height_km/2).destination(point=(center_lat, center_lon), bearing=180)
-    east_point = distance(kilometers=width_km/2).destination(point=(center_lat, center_lon), bearing=90)
-    west_point = distance(kilometers=width_km/2).destination(point=(center_lat, center_lon), bearing=270)
+    north_point = distance(
+        kilometers=height_km/2).destination(point=(center_lat, center_lon), bearing=0)
+    south_point = distance(
+        kilometers=height_km/2).destination(point=(center_lat, center_lon), bearing=180)
+    east_point = distance(
+        kilometers=width_km/2).destination(point=(center_lat, center_lon), bearing=90)
+    west_point = distance(
+        kilometers=width_km/2).destination(point=(center_lat, center_lon), bearing=270)
 
     # Extract the latitude and longitude from each point
     north_lat, _ = north_point.latitude, north_point.longitude
@@ -75,10 +96,15 @@ def create_bounding_box(center_lat, center_lon, width_km=3):
 
     # Create the bounding box
     bbox = box(west_lon, south_lat, east_lon, north_lat)
+
     return bbox
 
-def create_bounding_box_choropleth(lat, lon, width=3):
-    # Create Bbox
+
+def create_bounding_box_choropleth(lat: float,
+                                   lon: float,
+                                   width: float = 3) -> Tuple[Dict, go.Figure]:
+    """Create bounding box for a choropleth map."""
+
     bbox = create_bounding_box(lat, lon, width)
 
     # Create a DataFrame for Plotly Express
@@ -97,15 +123,16 @@ def create_bounding_box_choropleth(lat, lon, width=3):
 
     return bbox.__geo_interface__, fig
 
-def create_map(lat, lon, bbox):
-    # Extracting coordinates and transforming to the format folium expects
-    jsonCoords = bbox.__geo_interface__
-    coords = jsonCoords['coordinates'][0]
+
+def create_map(lat: float, lon: float, bbox: Tuple[float, float, float, float]) -> folium.Map:
+    """Extracting coordinates and transforming to the format folium expects."""
+
+    json_coords = bbox.__geo_interface__
+    coords = json_coords['coordinates'][0]
     folium_coords = [list(coord)[::-1] for coord in coords]  # Flip lon and lat for each coordinate
 
-    # print(f"Folium BBox Coords: {folium_coords}")
     # Get the bounds of the AOI polygon
-    polygon_shape = shape(jsonCoords)
+    polygon_shape = shape(json_coords)
     minx, miny, maxx, maxy = polygon_shape.bounds
     zoom_level = estimate_zoom_level(minx, miny, maxx, maxy)
     # print(zoom_level)
@@ -115,7 +142,13 @@ def create_map(lat, lon, bbox):
 
     return m_obj
 
-def create_choropleth_map(lat, lon, bbox_geojson, fig=None):
+
+def create_choropleth_map(lat: float,
+                          lon: float,
+                          bbox_geojson: Dict,
+                          fig: Optional[go.Figure] = None) -> go.Figure:
+    """Create thematic map to represent statistical data with shaded regions."""
+
     if fig is None:
         # Initialize a figure if it's not passed in
         fig = px.choropleth_mapbox(zoom=10, center=dict(lat=lat, lon=lon))
@@ -124,9 +157,9 @@ def create_choropleth_map(lat, lon, bbox_geojson, fig=None):
     coords = bbox_geojson['coordinates'][0]
     fig.add_shape(
         go.layout.Shape(
-            type="polygon",
+            type='polygon',
             coordinates=coords,
-            line=dict(width=2),
+            line={ "width": 2 }
         ),
         row=1,
         col=1
@@ -144,14 +177,17 @@ def create_choropleth_map(lat, lon, bbox_geojson, fig=None):
 
     # General update to layout
     fig.update_layout(
-        mapbox_style="carto-positron",
+        mapbox_style='carto-positron',
         mapbox_zoom=9,
         mapbox_center={"lat": lat, "lon": lon}
     )
 
     return fig
 
-def update_map_with_tiles(folium_map_obj, tiles_gdf, animation_filename, aoi_bbox):
+
+def update_map_with_tiles(folium_map_obj: folium.Map, tiles_gdf, animation_filename, aoi_bbox) -> folium.Map:
+    """Update a map with new folium polygon objects."""
+
     if tiles_gdf.empty:
         print("No items found.")
         return None  # or however you want to handle an empty response
@@ -173,7 +209,7 @@ def update_map_with_tiles(folium_map_obj, tiles_gdf, animation_filename, aoi_bbo
                 ).add_to(folium_map_obj)
             else:
                 print(f'Unsupported geometry type: {geometry.geom_type}')
-                exit()
+                sys.exit()
 
     # Calculate centroid of the bbox
     min_lon, min_lat, max_lon, max_lat = aoi_bbox.bounds
@@ -184,21 +220,40 @@ def update_map_with_tiles(folium_map_obj, tiles_gdf, animation_filename, aoi_bbo
     folium.Marker([centroid_lat, centroid_lon], popup_html, parse_html=True).add_to(folium_map_obj)
     return folium_map_obj
 
-def process_multiple_points_to_bboxs(points, width):
-    master_map = folium.Map(location=[points[0]['lat'], points[0]['lon']], zoom_start=13)  # Set initial location
+
+def process_multiple_points_to_bboxs(
+        points: List[Dict(str, float)],
+        width: float) -> Tuple[folium.Map, List[Tuple[float, float, float, float]]]:
+    """Add bounding boxes to a list of search areas.
+
+    Returns both the master map and a list of a AOIs.
+    """
+
+    # Set initial location
+    master_map = folium.Map(
+        location=[points[0]['lat'], points[0]['lon']], zoom_start=13)
+
     aois_list = []
-    for index, point in enumerate(points):  # Use enumerate to get the index and point
+    for point in points:
         lat, lon = point['lat'], point['lon']
         aoi = create_bounding_box(lat, lon, width)
         folium_map_search = create_map(lat, lon, aoi)
-        # Add the bounding box polygon to the list of search areas.
-        aois_list.append(aoi)  # Use append to add the aoi to the list
-        for feature in folium_map_search._children.values():
-            master_map.add_child(feature)  # Add each feature from the individual map to the master map
-    return master_map, aois_list  # Return both the master map and the list of AOIs
 
-def process_multiple_points_choropleth(points, width):
-    # Initialize the "master" Plotly figure
+        # Add the bounding box polygon to the list of search areas.
+        aois_list.append(aoi)
+        for feature in folium_map_search._children.values():
+            # Add each feature from the individual map to the master map
+            master_map.add_child(feature)
+
+    # Return both the master map and the list of AOIs
+    return master_map, aois_list
+
+
+def process_multiple_points_choropleth(
+        points: List[Dict[str, float]],
+        width: float) -> Tuple[go.Figure, List[Dict]]:
+    """Initialize the "master" Plotly figure."""
+
     master_fig = go.Figure()
 
     aois_list = []
@@ -226,18 +281,22 @@ def process_multiple_points_choropleth(points, width):
     # print(f"Min xy, Max xy: {minx}:{miny},{maxx}:{maxy}")
     # Update the layout of the "master" figure
     master_fig.update_layout(
-        mapbox=dict(
-            style="carto-positron",
-            zoom=zoom_level,
-            center=dict(lat=points[0]['lat'], lon=points[0]['lon']),
-        )
+        mapbox= {
+            "style": 'carto-positron',
+            "zoom": zoom_level,
+            "center": {
+                "lat": points[0]['lat'],
+                "lon": points[0]['lon'],
+            },
+        }
     )
 
     return master_fig, aois_list
 
 
+def create_heatmap_for_age(aggregated_gdf: Dict) -> folium.Map:
+    """Creates a heat map based on age of data, using a linear color map."""
 
-def create_heatmap_for_age(aggregated_gdf):
     # Sort the GeoDataFrame based on data_age, so that less old squares are on top
     aggregated_gdf = aggregated_gdf.sort_values(by='data_age', ascending=False)
 
@@ -262,29 +321,36 @@ def create_heatmap_for_age(aggregated_gdf):
     #                              color=colormap(row['data_age']),
     #                              fill=True).add_to(m)
     for idx, row in aggregated_gdf.iterrows():
-            # Scaling opacity: younger squares more opaque (0.8), older squares less opaque (0.4)
-            opacity_scaled = 0.8 - ((row['data_age'] - data_age_min) / (data_age_max - data_age_min)) * 0.4
+        # Scaling opacity: younger squares more opaque (0.8), older squares less opaque (0.4)
+        opacity_scaled = 0.8 - \
+            ((row['data_age'] - data_age_min) /
+             (data_age_max - data_age_min)) * 0.4
 
-            tooltip_text = f"Age: {row['data_age']}"
-            tooltip = folium.Tooltip(tooltip_text)
-            polygon = folium.Polygon(
-                locations=[(y, x) for x, y in zip(row.geometry.exterior.xy[0], row.geometry.exterior.xy[1])],
-                color=colormap(row['data_age']),
-                fill=True,
-                fill_color=colormap(row['data_age']),  # Fill color specified here
-                fill_opacity=opacity_scaled,  # Adjust fill opacity here
-                opacity=opacity_scaled,  # Adjust border opacity here
-                tooltip=tooltip # Add tooltip of age of data.
-            ).add_to(m)
+        tooltip_text = f"Age: {row['data_age']}"
+        tooltip = folium.Tooltip(tooltip_text)
+        folium.Polygon(
+            locations=[(y, x) for x, y in zip(
+                row.geometry.exterior.xy[0], row.geometry.exterior.xy[1])],
+            color=colormap(row['data_age']),
+            fill=True,
+            fill_color=colormap(row['data_age']),
+            fill_opacity=opacity_scaled,
+            opacity=opacity_scaled,
+            tooltip=tooltip
+        ).add_to(m)
 
     m.add_child(colormap)  # Add the color map legend
     now = datetime.now().strftime("%h-%m-%dT%H%M%SZ")
     filename = f'maps/Heatmap_Image_Age_{now}.html'
-    m.save(filename)  # Save to an HTML file
+    m.save(filename)
     logger.warning(filename)
+
     return m
 
-def create_heatmap_for_image_count(aggregated_gdf):
+
+def create_heatmap_for_image_count(aggregated_gdf: Dict) -> folium.Map:
+    """Creates a heat map based on quantity of available data using a linear color map."""
+
     # # Sort the GeoDataFrame based on image_count
     # aggregated_gdf = aggregated_gdf.sort_values(by='image_count', ascending=True)
 
@@ -297,7 +363,6 @@ def create_heatmap_for_image_count(aggregated_gdf):
     #     print("First Row of Data:", aggregated_gdf.iloc[0].to_dict())
     # else:
     #     print("The DataFrame is empty.")
-
 
     # Keep only the latest tile for each grid code.
     # Since they are sorted by age with the youngest last, we can drop duplicates except for the last one.
@@ -322,9 +387,8 @@ def create_heatmap_for_image_count(aggregated_gdf):
     for idx, row in aggregated_gdf.iterrows():
         tooltip_text = f"Image Count: {row['image_count']}"
         tooltip = folium.Tooltip(tooltip_text)
-        # logging.warning(f"ToolTip: {tooltip_text}")
 
-        polygon = folium.Polygon(
+        folium.Polygon(
             locations=[(y, x) for x, y in zip(row.geometry.exterior.xy[0], row.geometry.exterior.xy[1])],
             color=colormap(row['image_count']),
             fill=True,
@@ -339,9 +403,13 @@ def create_heatmap_for_image_count(aggregated_gdf):
     filename = f'maps/Heatmap_Image_Count_{now}.html'
     m.save(filename)  # Save to an HTML file
     logger.warning(filename)
+
     return m
 
-def create_heatmap_for_cloud(tiles_gdf, existing_fig=None):
+
+def create_heatmap_for_cloud(tiles_gdf: Dict, existing_fig: go.Figure = None) -> go.Figure:
+    """Creates a heat map based on cloud coverage in available data using a linear color map."""
+
     if tiles_gdf.empty:
         logger.warning("No items found.")
         return None  # or however you want to handle an empty response
@@ -360,7 +428,8 @@ def create_heatmap_for_cloud(tiles_gdf, existing_fig=None):
     centroid = Point(center_x, center_y)
 
     # Filter tiles by cloud cover
-    cloud_filtered_tiles_gdf = tiles_gdf[tiles_gdf['eo:cloud_cover'] <= config.CLOUD_THRESHOLD].copy()  # Added .copy() here
+    cloud_filtered_tiles_gdf = tiles_gdf[tiles_gdf['eo:cloud_cover']
+                                         <= config.CLOUD_THRESHOLD].copy()
 
     # Rest of your code remains the same
     cloud_filtered_tiles_gdf.sort_values(by='data_age', ascending=True, inplace=True)
@@ -395,7 +464,12 @@ def create_heatmap_for_cloud(tiles_gdf, existing_fig=None):
 
     return fig
 
-def create_folium_basemap(capture_grouped_tiles_gdf):
+
+def create_folium_basemap(capture_grouped_tiles_gdf: Dict) -> folium.Map:
+    """Create folium basemap as starting point for further updates with heatmaps
+    and other choropleths.
+    """
+
     if capture_grouped_tiles_gdf.empty:
         logging.warning("No Tiles Found")
         return None
